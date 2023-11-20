47e6d04cae7245c813c9e1b9ec421ebd8f9e17d6
unknown <danielpipet15@gmail.com>
2023-11-20 20:59:43 -0300

    Creacion de componente nosotros

--- TiendaOnline/src/app/app.module.ts
+++ TiendaOnline/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { FiltrosComponent } from './Components/filtros/filtros.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full'},
+  { path: 'inicio', component: InicioComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+];  
 
 @NgModule({
   declarations: [
@@ -14,9 +21,11 @@ import { FiltrosComponent } from './Components/filtros/filtros.component';
     FooterComponent,
     InicioComponent,
     FiltrosComponent,
+    NosotrosComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
